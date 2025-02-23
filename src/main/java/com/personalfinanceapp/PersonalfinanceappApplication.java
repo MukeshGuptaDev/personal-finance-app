4db559337bb682756c0d8c50f112c039ebef9248
Mukesh Kumar Gupta <mg7717736099@gmail.com>
2025-02-23 11:56:15 +0530

    init

---
+++ src/main/java/com/personalfinanceapp/PersonalfinanceappApplication.java
@@ -0,0 +1,13 @@
+package com.personalfinanceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalfinanceappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalfinanceappApplication.class, args);
+	}
+
+}
